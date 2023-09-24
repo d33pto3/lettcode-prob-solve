3ff6b56db19f4ec7c0120a513ec56eceb35ed1f6
deepToe <rakinfrhn@gmail.com>
2023-09-24 12:04:15 +0600

    35

---
+++ 35.ts
@@ -0,0 +1,19 @@
+function searchInsert(nums: number[], target: number): number {
+  let index = 0;
+  if (target < nums[0]) return 0;
+  if (target > nums[nums.length - 1]) return nums.length;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (target == nums[i]) {
+      index = i;
+      continue;
+    }
+    if (target > nums[i] && target < nums[i + 1]) {
+      index = i + 1;
+      continue;
+    }
+  }
+  return index;
+}
+
+console.log(searchInsert([1, 3, 5, 6], 7));
